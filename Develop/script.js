f9243687bf30cf6be33adcab7edd6baba586aacf
Van Taymany <vtaymany@gmail.com>
2020-11-07 20:08:14 -0800

    adds logic for changing colors of input boxes based on current time

--- Develop/script.js
+++ Develop/script.js
@@ -2,5 +2,20 @@
 var currentDate = moment().format('dddd, MMMM Do')
 $('#currentDay').text(currentDate)
 
-console.log(moment().format('HH'))
-console.log($('.current-hour').attr('data-hour'))
+var currentHour = parseInt(moment().format('HH'))
+var officeHour = $('.office-hour')
+
+for (i = 0; i < officeHour.length; i++) {
+  if (parseInt($(officeHour[i]).attr('data-hour')) == currentHour) {
+    var inputField = $(officeHour[i]).siblings().find('.event-input')
+    inputField.css('background-color', 'red')
+    inputField.attr('placeholder', 'Current Hour')
+  } else if (parseInt($(officeHour[i]).attr('data-hour')) > currentHour) {
+    var inputField = $(officeHour[i]).siblings().find('.event-input')
+    inputField.css('background-color', 'green')
+  }
+}
+
+$('.save-button').click(function () {
+  alert('The paragraph was clicked.')
+})
