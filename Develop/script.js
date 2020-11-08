6ef90afb1afa797b37ef9c087a2abe718bf50e85
Van Taymany <vtaymany@gmail.com>
2020-11-07 21:27:13 -0800

    adds save functionality

--- Develop/script.js
+++ Develop/script.js
@@ -17,5 +17,12 @@ for (i = 0; i < officeHour.length; i++) {
 }
 
 $('.save-button').click(function () {
-  alert('The paragraph was clicked.')
+  var populatedField = $(this).parent().siblings().find('.event-input').val()
+  var officeHour = $(this).parent().siblings('.office-hour').attr('data-hour')
+  console.log(officeHour)
+  localStorage.setItem(officeHour, populatedField)
 })
+
+// $(document).ready(function () {
+//   console.log('ready!')
+// })
