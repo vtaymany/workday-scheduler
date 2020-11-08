78fbb65a1e2d0a7bff499eb83f6e0abe6cca83c8
Van Taymany <vtaymany@gmail.com>
2020-11-07 23:10:56 -0800

    changes colors of the boxes to the correct hex

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,19 @@
 var currentDate = moment().format('dddd, MMMM Do')
 $('#currentDay').text(currentDate)
 
+//Variables to compare current time with timeblocks
 var currentHour = parseInt(moment().format('HH'))
 var officeHour = $('.office-hour')
 
+//
 for (i = 0; i < officeHour.length; i++) {
   if (parseInt($(officeHour[i]).attr('data-hour')) == currentHour) {
     var inputField = $(officeHour[i]).siblings().find('.event-input')
-    inputField.css('background-color', 'red')
+    inputField.css('background-color', '#ff6961')
     inputField.attr('placeholder', 'Current Hour')
   } else if (parseInt($(officeHour[i]).attr('data-hour')) > currentHour) {
     var inputField = $(officeHour[i]).siblings().find('.event-input')
-    inputField.css('background-color', 'green')
+    inputField.css('background-color', '#77dd77')
   }
 }
 
